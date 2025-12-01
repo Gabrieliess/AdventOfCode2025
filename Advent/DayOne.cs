21ee5a8cc5b9c5e3614037020f915d9a7f7104a6
Gabriel Galbavý <gabriel.galbavy9@gmail.com>
2025-12-01 22:22:15 +0100

    Cleanup, structure

---
+++ Advent/DayOne.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2025.Advent;
+
+partial class MyProgram
+{
+    public static int DayOne()
+    {
+        return 0;
+    }
+
+    public static int DayOnePlus()
+    {
+        return 1;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode2025.Advent;
+
+namespace AdventOfCode2025;
+
+public partial class LaunchPad
+{
+    public static void Main(string[] args)
+    {
+        Console.Write(MyProgram.DayOne());
+    }
+}

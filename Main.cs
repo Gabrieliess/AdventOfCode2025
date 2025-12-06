2085b9c586af64bc7c5dabc5b52ec5e2deb46bd5
Gabriel Galbavý <gabriel.galbavy9@gmail.com>
2025-12-06 15:38:28 +0100

    Day Four Solved

--- Main.cs
+++ Main.cs
@@ -7,7 +7,7 @@ public static class LaunchPad
     public static void Main(string[] args)
     {
         // select which day you want to solve
-        var result = MySolutions.SolveDayThreePlus();
+        var result = MySolutions.SolveDayFour();
         Console.WriteLine(result);
     }
 }
